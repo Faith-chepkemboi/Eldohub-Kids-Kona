9643160c9fa503c97d0822a8de1ffd9cda9bdc85
Tabkil2019 <chepkemoitoo3@gmail.com>
2022-10-17 17:04:34 +0300

    Eldohub Kids Kona

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,249 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="TemplateMo">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <title>EldoHub Kids Kona</title>
+   
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+
+    <link rel="stylesheet" href="assets/css/welcome.css">
+  </head>
+  
+  <body>
+  
+  
+    
+
+  <!-- Sub Header -->
+  <div class="sub-header">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-sm-8">
+          
+        </div>
+        <div class="col-lg-4 col-sm-4">
+          <div class="right-icons">
+            <ul>
+              <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+              <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+              <li><a href="#"><i class="fa fa-behance"></i></a></li>
+              <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+ 
+  <!-- ***** Header Area Start ***** -->
+  <section id="home">
+  <header class="header-area header-sticky" >
+      <div class="container">
+          <div class="row">
+        
+                  <nav class="main-nav">
+                    <!-- <img src="assets/images/logo.png" alt=""/> -->
+                  
+                      <ul class="nav">
+                          <li ><a href="#home" class="active">HOME</a></li>
+                          <li><a href="#abouut" class="active">ABOUT</a></li>
+                          <li ><a href="#offerr" class ="active">WHAT WE OFFER </a></li>
+                          <li ><a href="#" class="active" >OUR JOURNEY</a></li>
+                          <li><a href="#" class="active">TESTIMONIALS</a></li>
+                          <li><a href="#" class="active">CONTACT</a></li>
+                          
+
+                      </ul>        
+                      <a class='menu-trigger'>
+                          <span>Menu</span>
+                      </a>
+                      <!-- ***** Menu End ***** -->
+                      
+                    
+                  </nav>
+          
+                
+              </div>
+             
+          </div>
+          
+          
+      </div>
+      <!-- Section 1 of Main content -->
+      
+
+      
+  </header>
+  </section>
+  
+ 
+  <div class="containerr">
+    <div class="slides">
+      <div class="slide">
+      <img src="assets/images/meeting-01.jpg" alt="">
+      </div>
+      <div class="slide">
+      <img src="assets/images/meeting-02.jpg" alt="">
+      </div>
+      <div class="slide">
+      <img src="assets/images/meeting-03.jpg" alt="">
+      </div>
+      <div class="slide">
+      <img src="assets/images/meeting-04.jpg" alt="">
+      </div>
+
+    </div>
+    <div class="slide-controls">
+      <button id="prev-btn">
+        <i class="fa fa-chevron-left"></i>
+
+      </button>
+      <button class="next-btn">
+        <i class="fa fa-chevron-right"></i>
+      </button>
+      <section id="home">
+        <div class="welcome">
+            <h1 class="title">ELDOKIDZ</h1>
+            <h2 class="title2">YOUNG INNOVATORS CENTER FOR KIDS</h2>
+            <div class="button">
+              <button class="btn1">ENROLL ME</button>
+              <button class="btn2">READ MORE</button>
+            </div>
+          
+        </div> 
+     
+        
+    </section>
+    </div>
+    
+   <section id="abouut">
+  <div class="about">
+      <h6 class="bout"><b>About</b></h6>
+            <h7 style="font-size: 30px;">BootCamp Program</h7>
+            <br>
+            <p2 class="p2">Kids Bootcamp program inspires children between the age of 6-17 years to be innovative, creative and acquire problem-solving skills at an early age. With the current technology, there is a need to train these children to be the creators and not just consumers of technologies. Learning to code is not only just a way of setting a career opportunity but also the most straightforward way for children to boost their creativity and problem-solving skills.
+              <br>
+
+The program allows them to learn coding with easy block-based coding before they transition to real-world text-based programming languages. We allow them to participate by applying the principle that belongs to mathematics and develop strong mathematical thinking that will help them in many areas of their academic and personal life.</p2>
+                                     
+             
+  </div> 
+  <div class="div2">
+  <img src="assets/images/meeting-04.jpg"   alt=""/>
+                                     
+  </div>
+  
+  <div class="div3">
+  <h9 style="font-size: 30px;">Why kids BootCamp Program?</h9>
+  <p  class="p4">
+  Our main objective is to equip kids with 21st-century skills and knowledge to understand the current technology that they interact with daily. To achieve this we equip your kid with the following skills;
+
+  </p>
+  <br>
+                                     
+  </div>
+
+  
+   <div class="div4">
+   <!-- <img src="assets/images/meeting-01.jpg" alt="" style="width: 100px;"> -->
+   <i style="font-size:48px" class="fa">&#xf0e4;</i>
+   
+
+  <p style="font-size: 15px;">
+  Problem solving skills with mathematical understanding
+
+  </p>
+   </div>
+                                     
+  
+   <!-- </section> -->
+   <div class="div5">
+   <!-- <img src="assets/images/meeting-01.jpg" alt="" style="width: 100px;"> -->
+   <i style="font-size:48px" class="fa" >&#xf121;</i>
+  
+  <p style="font-size: 15px;">
+  promotes computational thinking, persistence and colloboration among the kids/learners
+
+  </p>
+   </div>
+   <div class="div6">
+   <!-- <img src="assets/images/meeting-01.jpg" alt="" style="width: 100px;"> -->
+   <i style="font-size:48px" class="fa">&#xf11b;</i>
+   
+   
+  <p style="font-size: 15px;">
+  Strives to promote soft skills rather than technicall skills
+
+  </p>
+   </div>
+  <div class="div7">
+        
+<i style="font-size:48px" class="fa" >&#xf121;</i>
+<p style="font-size: 15px;">A deeper understanding of mathematical concepts that comes in handy for academic execellence</p>
+        
+  
+   </div>
+   <div class="div8">
+
+   <i style="font-size:48px" class="fa" >&#xf121;</i>
+   <p style="font-size: 15px;">Coding teaches children how to think and solve daily problems</p>
+
+   </div>
+   <div class="div9">
+   <i style="font-size:50px" class="fa">&#xf11b;</i>
+   <p style="font-size: 15px; ">Kids create interactive games, stories, animations and solve fun puzzle which is powerful creative thinking and development of skills in problem solving</p>
+
+   </div>
+   <div>
+   <video  class="bg-video" controls>
+          <source src="assets/images/course-video.mp4" type="video/mp4" />
+
+          <h6>
+    How Kids Learn During The program
+    </h6>
+    <p style="font-size: 15px;">In the program Kids create interactive games, stories, animations and solve fun puzzle which is powerful creative thinking and development of skills in problem solving</p>
+    <button id="myBtn" onclick="myfunction()">Pause</button>
+      </video>
+   </div>
+   <!-- <div class="video-content">
+    <h6>
+    How Kids Learn During The program
+    </h6>
+    <p style="font-size: 15px;">In the program Kids create interactive games, stories, animations and solve fun puzzle which is powerful creative thinking and development of skills in problem solving</p>
+    <button id="myBtn" onclick="myfunction()">Pause</button>
+
+   </div> -->
+   
+   </section>
+   <section id="offerr">
+    <div class="offer">
+      <h1 class="offer1">
+      <b>WHAT WE OFFER</b>
+      </h1>
+      <h3 style="font-size: 30px; text-align:center"><b>Programs Available</b></h3>
+      <p4 class="p5">Kids BootCamp Program at Eldohub empowers children with powerful skills that help them design their own future through training them in core STEM fields. Following are some of the courses we offer for kids and teens;</p4>
+
+    </div>
+
+   </section>
+                                     
+  
+
+   
+<script src="assets/js/banner.js"></script>
+
+</body>
+
+</html>
